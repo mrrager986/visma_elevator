4d302957635b103a4d6f3e0c70eba687fb2bdc95
mrrager986 <mr.rager986@gmail.com>
2022-06-22 10:59:26 +0200

    script edit

--- script.js
+++ script.js
@@ -3,68 +3,82 @@
 $(function() {
     // prizemie
     $('#buttonGround').click(function() {
-        $("#liftOpen1").hide();
-        $("#liftClosed1").show();    
-        $("#liftOpenG").show();    
-        $("#liftClosedG").hide();    
-        $("#liftClosed2").show();    
-        $("#liftOpen2").hide();    
-        $("#liftOpen3").hide();    
-        $("#liftClosed3").show();    
-        $("#liftOpen4").hide();    
-        $("#liftClosed4").show();    
+        $("#liftOpen1, #liftClosedG, #liftOpen2, #liftOpen3, #liftOpen4").hide();
+        $("#liftClosed1, #liftOpenG, #liftClosed2, #liftClosed3, #liftClosed4").show();    
+        $("strong:contains('G')").css('color','black');
+        $("strong:contains('1')").css('color','white');
+        $("strong:contains('2')").css('color','white');
+        $("strong:contains('3')").css('color','white');
+        $("strong:contains('4')").css('color','white');    
+        $(".groundFloor").css('color','green');   
+        $(".floorOne").css('color','white');   
+        $(".floorTwo").css('color','white');   
+        $(".floorThree").css('color','white');   
+        $(".floorFour").css('color','white');     
     });
 
     // prve poschodie
     $('#button1').click(function() {
-        $("#liftOpenG").hide();
-        $("#liftClosedG").show();    
-        $("#liftOpen1").show();    
-        $("#liftClosed1").hide();    
-        $("#liftOpen2").hide();    
-        $("#liftClosed2").show();    
-        $("#liftClosed3").show();    
-        $("#liftOpen3").hide();    
-        $("#liftOpen4").hide();    
-        $("#liftClosed4").show();    
+        $("#liftOpenG, #liftClosed1, #liftOpen2, #liftOpen3, #liftOpen4").hide();
+        $("#liftClosedG, #liftOpen1, #liftClosed2, #liftClosed3, #liftClosed4").show();      
+        $("strong:contains('1')").css('color','black'); 
+        $("strong:contains('G')").css('color','white'); 
+        $("strong:contains('2')").css('color','white'); 
+        $("strong:contains('3')").css('color','white');
+        $("strong:contains('4')").css('color','white');   
+        $(".floorOne").css('color','green');  
+        $(".groundFloor").css('color','white');    
+        $(".floorTwo").css('color','white');   
+        $(".floorThree").css('color','white');   
+        $(".floorFour").css('color','white');     
     });
 
     // druhe poschodie,
     $('#button2').click(function() {
-        $("#liftOpen1").hide();
-        $("#liftClosed1").show();    
-        $("#liftOpen2").show();    
-        $("#liftClosed2").hide();    
-        $("#liftOpenG").hide();    
-        $("#liftClosedG").show();    
-        $("#liftClosed3").show();    
-        $("#liftOpen3").hide();    
-        $("#liftOpen4").hide();    
-        $("#liftClosed4").show();    
+        $("#liftOpen1, #liftClosed2, #liftOpenG, #liftOpen3, #liftOpen4").hide();
+        $("#liftClosed1, #liftOpen2, #liftClosedG, #liftClosed3, #liftClosed4").show();    
+        $("strong:contains('2')").css('color','black');
+        $("strong:contains('1')").css('color','white'); 
+        $("strong:contains('G')").css('color','white'); 
+        $("strong:contains('3')").css('color','white'); 
+        $("strong:contains('4')").css('color','white');   
+        $(".floorTwo").css('color','green');  
+        $(".groundFloor").css('color','white');   
+        $(".floorOne").css('color','white');     
+        $(".floorThree").css('color','white');   
+        $(".floorFour").css('color','white');             
     });
     
     // tretie poschodie
     $('#button3').click(function() {
-        $("#liftOpen2").hide();
-        $("#liftClosed2").show();    
-        $("#liftOpen3").show();    
-        $("#liftClosed3").hide();    
-        $("#liftOpenG").hide();    
-        $("#liftClosedG").show();    
-        $("#liftClosed4").show();    
-        $("#liftOpen4").hide();    
+        $("#liftOpen2, #liftClosed3, #liftOpenG, #liftOpen4").hide(); //tu nieje #liftOpen1, pretoze vytah je zatvoreny uz po predchadzajucom ukone
+        $("#liftClosed2, #liftOpen3, #liftClosedG, #liftClosed4").show();  
+        $("strong:contains('3')").css('color','black');
+        $("strong:contains('1')").css('color','white');
+        $("strong:contains('2')").css('color','white'); 
+        $("strong:contains('G')").css('color','white'); 
+        $("strong:contains('4')").css('color','white');   
+        $(".floorThree").css('color','green'); 
+        $(".groundFloor").css('color','white');   
+        $(".floorOne").css('color','white');   
+        $(".floorTwo").css('color','white');    
+        $(".floorFour").css('color','white');          
     });
     
     // stvrte poschodie
     $('#button4').click(function() {
-        $("#liftOpen3").hide();
-        $("#liftClosed3").show();    
-        $("#liftOpen4").show();    
-        $("#liftClosed4").hide();    
-        $("#liftClosed1").show();    
-        $("#liftOpen1").hide();    
-        $("#liftClosed2").show();    
-        $("#liftOpen2").hide();    
+        $("#liftOpen3, #liftClosed4, #liftOpen1, #liftOpen2").hide(); //tu nieje #liftOpenG, pretoze vytah je zatvoreny uz po predchadzajucom ukone
+        $("#liftClosed3, #liftOpen4, #liftClosed, #liftClosed2, #liftClosed1").show();       
+        $("strong:contains('4')").css('color','black');
+        $("strong:contains('1')").css('color','white'); 
+        $("strong:contains('2')").css('color','white'); 
+        $("strong:contains('3')").css('color','white'); 
+        $("strong:contains('G')").css('color','white'); 
+        $(".floorFour").css('color','green');  
+        $(".groundFloor").css('color','white');   
+        $(".floorOne").css('color','white');   
+        $(".floorTwo").css('color','white');   
+        $(".floorThree").css('color','white');              
     });
 
 });
